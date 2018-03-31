f6d72798cd2b25162a68cc9110c28a49e520f800
prayogorizkymaulany <prayogo0907@gmail.com>
2018-03-31 20:56:55 +0800

    membuat attribute dan behavior

--- src/praktek2/pembelianairbotol.java
+++ src/praktek2/pembelianairbotol.java
@@ -1,4 +1,12 @@
 package praktek2;
 public class pembelianairbotol {
+    int hargasatuan;
+    int totalpembelian;
+    
+    void cetakInfo(){
+        System.out.println("Pembelian Air Botol========");
+        System.out.println("Harga Satuan    : "+hargasatuan);
+        System.out.println("Total Pembelian : "+totalpembelian);
+    }
     
 }
