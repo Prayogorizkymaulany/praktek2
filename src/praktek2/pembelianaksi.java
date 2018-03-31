1316010104d56620521eb3bfa2df27838f000775
prayogorizkymaulany <prayogo0907@gmail.com>
2018-03-31 21:23:25 +0800

    perbaikan instance objeck

--- src/praktek2/pembelianaksi.java
+++ src/praktek2/pembelianaksi.java
@@ -4,7 +4,7 @@ public class pembelianaksi {
     public static void main(String[] args) {
         pembelianairbotol botol1 = new pembelianairbotol();
         botol1.hargasatuan = 3000;
-        botol1.totalpembelian = 5;
+        botol1.jumlahpembelian = 5;
         
         botol1.cetakInfo();
         
